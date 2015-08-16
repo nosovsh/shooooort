2d593ad265c7665c1e69614deb6ea943c1290a87
trashgenerator <trashgenerator@gmail.com>
2015-08-16 12:32:21 +0300

    logging

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import App from 'Components/App/App';
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from 'Flux/reducers';
 
-const store = createStore(rootReducer);
+/**
+ * Logs all actions and states after they are dispatched.
+ */
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd(action.type);
+  return result;
+};
+
+let createStoreWithMiddleware = applyMiddleware(
+  logger
+)(createStore);
+
+let store = createStoreWithMiddleware(rootReducer);
 
 export default class Root extends Component {
   render() {
